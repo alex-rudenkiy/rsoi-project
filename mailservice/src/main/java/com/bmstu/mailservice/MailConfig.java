cca8d617efd1887bda43547f99c949063c76eef2
Alex Rudenkiy <alex-rudenkiy@mail.ru>
2022-05-27 21:23:51 +0300

    Незначительные правки

--- mailservice/src/main/java/com/bmstu/mailservice/MailConfig.java
+++ mailservice/src/main/java/com/bmstu/mailservice/MailConfig.java
@@ -33,6 +33,7 @@ public class MailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
